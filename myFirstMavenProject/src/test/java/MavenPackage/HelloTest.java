867172ae7d10d7d6d5cb707dc270c53577a70ec4
AKS <a161577@MC0R2F2C.ww930.my-it-solutions.net>
2015-09-13 11:52:48 +0100

    added Maven & TestNG

--- myFirstMavenProject/src/test/java/MavenPackage/HelloTest.java
+++ myFirstMavenProject/src/test/java/MavenPackage/HelloTest.java
@@ -1,5 +1,6 @@
 package MavenPackage;
 
+import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
